49111702285fc28fc979d71f84352715e84466e7
tonypinkevych <tony@aninix.com>
2024-04-29 09:48:06 +0200

    feat: add progress for other commands

--- src/resize.tsx
+++ src/resize.tsx
@@ -1,4 +1,4 @@
-import { Toast, showToast } from "@raycast/api";
+import { Toast, showToast, updateCommandMetadata } from "@raycast/api";
 import * as path from "path";
 import { Ffmpeg } from "./objects/ffmpeg";
 import { Ffprobe } from "./objects/ffprobe";
@@ -7,54 +7,58 @@ import { SelectedFinderFiles } from "./objects/selected-finder.videos";
 import { Video } from "./objects/video";
 
 export default async function Command(props: { arguments: { width: string; height: string } }) {
-  const { width: providedWidth, height: providedHeight } = props.arguments;
+  try {
+    const { width: providedWidth, height: providedHeight } = props.arguments;
 
-  if (!providedWidth && !providedHeight) {
-    await showToast({ title: "Width or Height should be proivded", style: Toast.Style.Failure });
-    return;
-  }
+    if (!providedWidth && !providedHeight) {
+      await showToast({ title: "Width or Height should be proivded", style: Toast.Style.Failure });
+      return;
+    }
 
-  const files = await new SelectedFinderFiles().list();
+    const files = await new SelectedFinderFiles().list();
 
-  if (files.length === 0) {
-    await showToast({ title: "Please select any video in Finder", style: Toast.Style.Failure });
-    return;
-  }
+    if (files.length === 0) {
+      await showToast({ title: "Please select any video in Finder", style: Toast.Style.Failure });
+      return;
+    }
 
-  const ffmpeg = new Ffmpeg(
-    new Ffprobe({
-      onStatusChange: async (status) => {
-        await showToast({ title: status, style: Toast.Style.Animated });
+    const ffmpeg = new Ffmpeg(
+      new Ffprobe({
+        onStatusChange: async (status) => {
+          await showToast({ title: status, style: Toast.Style.Animated });
+        },
+      }),
+      {
+        onStatusChange: async (status) => {
+          await showToast({ title: status, style: Toast.Style.Animated });
+        },
+        onProgressChange: (progress) => {
+          updateCommandMetadata({ subtitle: `${Math.round(progress * 100)}%` });
+        },
       },
-    }),
-    {
-      onStatusChange: async (status) => {
-        await showToast({ title: status, style: Toast.Style.Animated });
-      },
-      onProgressChange: (progress) => {
-        console.log(">>>", progress);
-      },
-    },
-  );
+    );
 
-  for (const file of files) {
-    const width = providedWidth !== "" ? parseInt(providedWidth, 10) : undefined;
-    const height = providedHeight !== "" ? parseInt(providedHeight, 10) : undefined;
+    for (const file of files) {
+      const width = providedWidth !== "" ? parseInt(providedWidth, 10) : undefined;
+      const height = providedHeight !== "" ? parseInt(providedHeight, 10) : undefined;
 
-    try {
       const extension = path.extname(file.path());
       if (extension === ".gif") {
         await new Gif(file, ffmpeg).encode({ width, height });
       } else {
         await new Video(file, ffmpeg).encode({ width, height });
       }
-    } catch (err) {
-      if (err instanceof Error) {
-        await showToast({ title: err.message, style: Toast.Style.Failure });
-      }
-      return;
     }
-  }
 
-  await showToast({ title: "All videos processed", style: Toast.Style.Success });
+    await showToast({ title: "All videos processed", style: Toast.Style.Success });
+    updateCommandMetadata({ subtitle: "Finished Sucessfully" });
+  } catch (err) {
+    if (err instanceof Error) {
+      updateCommandMetadata({ subtitle: "Finished with Error" });
+    }
+  } finally {
+    setTimeout(() => {
+      updateCommandMetadata({ subtitle: null });
+    }, 1000);
+  }
 }
