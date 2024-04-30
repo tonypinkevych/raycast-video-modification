967164b7e198dd7c57a8b59f396c5cd488e8a867
tonypinkevych <tony@aninix.com>
2024-04-30 22:55:16 +0200

    chore: show hud instead of toast when processing finished

--- src/convert.tsx
+++ src/convert.tsx
@@ -1,4 +1,4 @@
-import { Toast as RaycastToast } from "@raycast/api";
+import { Toast as RaycastToast, showHUD } from "@raycast/api";
 import { Ffmpeg } from "./objects/ffmpeg";
 import { Ffprobe } from "./objects/ffprobe";
 import { Gif } from "./objects/gif";
@@ -42,7 +42,7 @@ export default async function Command(props: { arguments: { format: "mp4" | "web
       await new Video(file, ffmpeg).encode({ format });
     }
 
-    await toast.show({ title: "All Videos are Processed", style: RaycastToast.Style.Success });
+    await showHUD("All Videos are Processed");
   } catch (err) {
     if (err instanceof FinderIsNotFrontmostApp) {
       await toast.show({ title: "Please put Finder in focus and try again", style: RaycastToast.Style.Failure });
